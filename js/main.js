1d6f02fa514374e19a29c34cd83777ba4588f5ee
reginaldodlneto <reginaldo.dlneto@gmail.com>
2021-07-20 19:30:06 -0300

    adicionando a funcao de concluir tarefas

--- js/main.js
+++ js/main.js
@@ -6,20 +6,27 @@ const Main = {
 
   cacheSelectors: function () {
     // Usar o this para ela ficar disponivel para tosos os elementos
-    this.checkButtons = document.querySelectorAll('.check')
+    this.$checkButtons = document.querySelectorAll('.check')
   },
 
   bindEvents: function () {
     const self = this
 
-    this.checkButtons.forEach(function (button) {
+    this.$checkButtons.forEach(function (button) {
       button.onclick = self.Events.checkButton_click
     })
   },
 
   Events: {
-    checkButton_click: function () {
-      alert('ok')
+    checkButton_click: function (e) {
+      const li = e.target.parentElement
+      const isDone = li.classList.contains('done')
+
+      if (!isDone) {
+        return li.classList.add('done')
+      }
+
+      li.classList.remove('done')
     }
   }
 }
